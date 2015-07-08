8a1aa40253b673fc5acc10cfddf32c71ebb34c9a
tropicloud <admin@tropicloud.net>
2015-07-08 19:31:27 -0300

    Wed Jul  8 19:31:27 BRT 2015

--- wps/bin/wps_error.sh
+++ wps/bin/wps_error.sh
@@ -2,23 +2,39 @@
 # ERROR
 # ---------------------------------------------------------------------------------
 
+wps_error_header() {
+	echo -e "\033[0;30m
+-----------------------------------------------------
+\033[1;31m  Error $T1\033[0m -\033[1;37m $T2\033[0;30m
+-----------------------------------------------------"
+}
+
 wps_error() {
 
-	if [[  $1 == '401'  ]]; then echo -e "\033[0;30m
------------------------------------------------------
-\033[1;31m  (Error 401)\033[0m -\033[1;37m WP_DOMAIN not found\033[0;30m
------------------------------------------------------
-\033[0m
+	if [[  $1 == '401'  ]]; then
+	
+		T1='401'
+		T2='WP_DOMAIN not found'
+		
+		wps_error_header && echo -e "\033[0m
 The environment variable WP_DOMAIN do not exists.
 Pleae define WP_DOMAIN in your 'docker run' command.
-Example:
-  
-  docker run -p 80:80 -e WP_DOMAIN=\"example.com\" -d tropicloud/submarine
 
-Aborting...
-";  fi
+Ex: docker run -p 80:80 -e WP_DOMAIN=\"example.com\" -d tropicloud/submarine
+\033[0m"
+	fi
 
 
 
+
+
+
+
+
+
+
+
+
+echo -e "Aborting...\n"
 exit 1;
 }
