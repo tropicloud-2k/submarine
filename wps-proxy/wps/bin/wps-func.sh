84e30ee7342a237606d88d7a5f76d951f4c7a812
tropicloud <admin@tropicloud.net>
2015-07-19 22:22:07 -0300

    Sun Jul 19 22:22:07 BRT 2015

--- wps-proxy/wps/bin/wps-func.sh
+++ wps-proxy/wps/bin/wps-func.sh
@@ -31,8 +31,7 @@ wps_events() {
 	events="/tmp/events.json"
 	
 	while inotifywait -e modify $events; do
-	    if tail -n1 $events | grep ':"exec_start'; then wps_reload 2>&1 &
-	  elif tail -n1 $events | grep ':"restart'; then wps_reload 2>&1 &
+	    if tail -n1 $events | grep ':"restart'; then wps_reload 2>&1 &
 	  elif tail -n1 $events | grep ':"start'; then wps_reload 2>&1 &
 	  elif tail -n1 $events | grep ':"stop'; then wps_reload 2>&1 &
 	  fi
